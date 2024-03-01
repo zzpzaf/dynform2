30b98e0eb7b7b39227933abc95a03ff1a9695e89
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-03-01 11:54:08 +0100

    15th commit The router and the router-outlet tags are not necessary in the AppComponent so it has been refactored accordingly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
-import { RequestDataComponent } from './request-data/request-data.component';
-import { AboutComponent } from './about/about.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
+    // RouterOutlet,
     HomeComponent,
-    RequestDataComponent,
-    AboutComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
